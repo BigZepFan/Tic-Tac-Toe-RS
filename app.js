e151b6ac3fb5cf69bc6263bc0c6adf4c6bbec288
bigzepfan <dlcandassociates@gmail.com>
2022-07-14 19:40:12 -0600

    You need to start over

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
-const board = [
-  [1, 1, 1],
-  [2, 2, 2],
-  [3, 3, 3],
-  
+let input = document.getElementById("input");
+let input2 = document.getElementById("input2");
+function ticPlayers() {
+  let button = document.querySelector("#submit-button");
+}
+button.addEventListener("click", function (e) {
+  const player1 = input.value;
+  const player2 = input2.value;
+  player1.innerText = player1.input;
+  player2.innerText = player2.input2;
+
+  let gameState = {
+    board: [null, null, null, null, null, null, null, null, null],
+    currentPlayer: "X",
+  };
+  const board = document.querySelector("#board");
+
+  board.addEventListener("click", function (e) {
+    const index = +e.target.id;
+
+    renderBoard();
+
+    gameState.board[index] = gameState.currentPlayer;
+    console.log(gameState.board);
+    function renderBoard() {
+      for (let i = 0; i < gameState.board.length; i++) {
+        const currDiv = document.getElementById(`${i}`);
+        currDiv.innerText = gameState.board[i];
+      }
+    }
+  });
+  function switchPlayer() {
+    if (gameState.currentPlayer === "X") {
+      gameState.currentPlayer = "O";
+    } else {
+      gameState.currentPlayer = "X";
+    }
+  }
+});
+
+switchPlayer();
